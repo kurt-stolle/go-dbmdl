002d619a5d140406a4864788dbbb56b2c5cdfc39
Kurt Stolle <kurt@casualbananas.com>
2017-06-12 10:06:53 +0200

    Fixed bug where struct is accessed as pointer

--- model_patch.go
+++ model_patch.go
@@ -8,21 +8,26 @@ import (
 // Patch is used when there is no struct initialization required, e.g. when only fields in the database need to be updated.
 func (m *Model) Patch(where WhereSelector, update map[string]interface{}) error {
 	// Create a new struct so that we can pass this to Save
-	var newStruct = reflect.New(m.Type)
+	var typePtr = reflect.New(m.Type)
 	var fields []string
 
 	// Verify the map's entries
 	for k, v := range update {
 		if _, found := m.Type.FieldByName(k); !found {
-			return ErrNotFound
+			continue // Skip fields that don't exist
 		}
 
-		field := newStruct.FieldByName(k)
+		field := typePtr.Elem().FieldByName(k)
 		field.Set(reflect.ValueOf(v))
 
 		fields = append(fields, k)
 	}
 
+	// Check whether we've actually updated anything
+	if len(fields) < 1 {
+		return nil
+	}
+
 	// Perform Save
-	return m.Save(newStruct.Addr().Interface(), where, fields...)
+	return m.Save(typePtr.Interface(), where, fields...)
 }
